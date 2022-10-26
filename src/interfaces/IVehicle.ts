e3e0c69ed5cf2dada5f8f6dd288b810e6a132aa6
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-25 20:14:17 -0500

    feat: Cria a interface IVehicle genérica

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,13 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().max(2022).min(1900),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
+
+export type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export default vehicleZodSchema;
