631252e8e0dd2129f6d70de4b51da3df8598a7aa
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-26 01:35:21 -0500

    feat: cria aquivo para centralizar rotas

---
+++ src/routes/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import Cars from '../models/CarModel';
+import CarService from '../services/CarService';
+
+const route = Router();
+
+const cars = new Cars();
+const carService = new CarService(cars);
+const carController = new CarController(carService);
+
+route.post('/cars', (req, res) => carController.creat(req, res));
+
+export default route;
