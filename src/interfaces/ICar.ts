12018cedf9d42a73405f190c688d23b03975d42f
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-26 01:30:02 -0500

    feat: cria interface ICar

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,11 +1,11 @@
 import { z } from 'zod';
 import { vehicleZodSchema } from './IVehicle';
 
-const carZodSchema = vehicleZodSchema.extend({
+const CarZodSchema = vehicleZodSchema.extend({
   doorsQty: z.number().max(3).min(2),
   seatsQty: z.number().max(7).min(2),
 });
 
-export type ICar = z.infer<typeof carZodSchema>;
+type ICar = z.infer<typeof CarZodSchema>;
 
-export default carZodSchema;
+export { CarZodSchema, ICar };
