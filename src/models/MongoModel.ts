bab572b7a1fb7fba3ca0ca95297bd768da9cde1c
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-26 01:34:17 -0500

    feat: cria um model generico

---
+++ src/models/MongoModel.ts
@@ -0,0 +1,36 @@
+import { isValidObjectId, Model } from 'mongoose';
+import { ErrorTypes } from '../errors/catalog';
+import { IModel } from '../interfaces/IModel';
+
+abstract class MongoModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+  
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+
+  public async create(obj: T): Promise<T> {
+    return this._model.create({ ...obj });
+  }
+
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
+
+  public async readOne(_Id: string): Promise<T | null> {
+    if (!isValidObjectId(_Id)) throw Error('InvalidMongoId');
+    return this._model.findOne({ _Id });
+  }
+
+  public async update(_Id: string, obj: T): Promise<T | null> {
+    if (!isValidObjectId(_Id)) throw Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndUpdate({ _Id, obj });
+  }
+
+  public async delete(_Id: string): Promise<T | null> {
+    if (!isValidObjectId(_Id)) throw Error(ErrorTypes.InvalidMongoId);
+    return this._model.findByIdAndDelete({ _Id });
+  }
+}
+
+export default MongoModel;
