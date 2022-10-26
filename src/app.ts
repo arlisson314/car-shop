7f322a9ff360a5c859a587ee34295fe869c935d3
Arlisson N. Costa <arlisson314@gmail.com>
2022-10-26 01:23:12 -0500

    feat: cria endpoint para rotas

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './errors/errorHandler';
+import route from './routes/routes';
 
 const app = express();
+app.use(express.json());
+app.use(route);
+app.use(errorHandler);
 
 export default app;
